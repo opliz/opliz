d87f22e9cc5cd23209570ecd59aaab0c051a51c6
Cuong Nguyen <cuong_nguyen@hotmail.com>
2017-12-21 11:46:26 +0700

    test

--- src/app/common/beans/chapter.ts
+++ src/app/common/beans/chapter.ts
@@ -1,6 +1,9 @@
+import { Story } from './story';
+
 export class Chapter {
   chapterId: number;
   chapterName : string;
   chapterIndex : string;
   chapterCoverImageUrl : string;
+  story : Story;
 }
