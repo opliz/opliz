bf6415876a60e6031d0b86401b90c52847bc8edd
Cuong Nguyen <cuong_nguyen@hotmail.com>
2017-12-22 14:41:53 +0700

    test

--- src/app/routing.ts
+++ src/app/routing.ts
@@ -1,10 +1,9 @@
 import { RouterModule, Routes } from '@angular/router';
-import { HomeLayoutComponent } from './layout/home/home-layout.component';
-import { ContentLayoutComponent } from './layout/content/content-layout.component';
-import { HomeComponent } from './components/home/home.component';
-import { AboutComponent } from './components/about/about.component';
-import { ContactComponent } from './components/contact/contact.component';
-import { TestComponent } from './components/test/test.component';
+// import { HomeLayoutComponent } from './layout/home/home-layout.component';
+// import { ContentLayoutComponent } from './layout/content/content-layout.component';
+// import { HomeComponent } from './components/home/home.component';
+// import { AboutComponent } from './components/about/about.component';
+// import { ContactComponent } from './components/contact/contact.component';
 
 export const appRoutes: Routes = [
   // { path: '', component: LayoutComponent, loadChildren: 'app/layout/layout.module#LayoutModule' },
