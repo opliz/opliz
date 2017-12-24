ff5be38e5a8775a894df7c3807c5f066d461ae0c
Cuong Nguyen <cuong_nguyen@hotmail.com>
2017-12-24 22:39:03 +0700

    test

--- src/app/components/more/more.component.ts
+++ src/app/components/more/more.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router ,ActivatedRoute} from '@angular/router';
+import { Chapter } from '../../common/beans/chapter';
+import { MoreService } from './more.service';
 
 @Component({
   selector: 'app-more',
@@ -7,9 +10,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MoreComponent implements OnInit {
 
-  constructor() { }
+  index : number;
+  activeIdSub: any;
+  chapters : Array<Chapter>;
+
+  constructor(private moreService : MoreService, private router : Router,private route : ActivatedRoute) {
+    this.activeIdSub = this.route.params.subscribe(params => {
+      this.index = params['index'];
+      this.chapters = new Array<Chapter>();
+    });
+  }
 
   ngOnInit() {
+    this.loadChapters();
+    this.index++;
+  }
+
+  loadChapters() {
+    var request = this.moreService.getChaptesBySize(this.index).subscribe(result => {
+      this.chapters = result;
+    });
   }
 
 }
