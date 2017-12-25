51196a1deeb0c4132cc825f7eba5a9b431ec5a33
Cuong Nguyen <cuong_nguyen@hotmail.com>
2017-12-25 13:29:09 +0700

    test

--- src/app/components/story/story.component.ts
+++ src/app/components/story/story.component.ts
@@ -12,20 +12,29 @@ export class StoryComponent implements OnInit {
   storyId : number;
   activeIdSub: any;
   story : Story;
+  relatedStories: Array<Story>;
   constructor(private storyService : StoryService, private router: Router,private route: ActivatedRoute) {
     this.activeIdSub = this.route.params.subscribe(params => {
       this.storyId = params['storyId'];
       this.story = new Story();
+      this.relatedStories = new Array<Story>();
     });
   }
 
   ngOnInit() {
     this.loadStory();
+    this.loadRelatedStories();
   }
 
   loadStory() {
     var request = this.storyService.getStoryById(this.storyId).subscribe(result => {
       this.story = result;
+    });
+  }
+
+  loadRelatedStories() {
+    var request = this.storyService.getRelatedStoriesById(this.storyId).subscribe(result => {
+      this.relatedStories = result;
     });
   }
 }
--- src/app/components/story/story.service.ts
+++ src/app/components/story/story.service.ts
@@ -6,11 +6,19 @@ import { BaseService } from '../../common/services/base.service';
 @Injectable()
 export class StoryService extends BaseService {
   urlStoriesApi : string = Config.apiUrl + '/webservice/manga/story?manga_id=';
+  urlRelatedStoriesApi : string = Config.apiUrl + '/webservice/manga/stories/related?size=7&manga_id=';
   constructor(public http : HttpInterceptor) {
     super(http);
   }
   getStoryById(id) {
     var url = this.urlStoriesApi+id;
+    return this.http.get(url, this.getHeaders()).map((response: Response) =>{
+      var result = response.json();
+      return result;
+    });
+  }
+  getRelatedStoriesById(id) {
+    var url = this.urlRelatedStoriesApi+id;
     return this.http.get(url, this.getHeaders()).map((response: Response) =>{
       var result = response.json();
       return result;
