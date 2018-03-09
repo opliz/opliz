c19f0f52779b27ae9365463274ce17262737a626
opliz <32351129+opliz@users.noreply.github.com>
2018-03-09 14:12:57 +0700

    test

--- src/assets/js/site/site.js
+++ src/assets/js/site/site.js
@@ -7,4 +7,15 @@ $(document).ready(function() {
 	$("#btnCancel").click(function() {
 		window.history.back();
 	});
+
+	// var url = window.location.href;
+	$("div.widget-content > ul#manga-type > li > a").each(function() {
+		// var menuUrl = this.href;
+		// var lastSlashMenuIndex = menuUrl.lastIndexOf("/");
+		// var checkUrl = menuUrl.substring(0, lastSlashMenuIndex);
+		// if(url.indexOf(checkUrl) !== -1) {
+		//  $(this).closest("li").addClass("side-bar-active");
+		// }
+	  console.log("index");
+	});
 });
