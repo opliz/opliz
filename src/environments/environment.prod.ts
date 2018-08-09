966f55f107504c81f4809f5206ffa89b67302c2f
opliz <32351129+opliz@users.noreply.github.com>
2018-08-09 09:46:56 +0700

    test

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,3 @@ export const environment = {
   production: true,
   api : "https://app-encee.7e14.starter-us-west-2.openshiftapps.com"
 };
-
-// api : "http://app-opliz.1d35.starter-us-east-1.openshiftapps.com"
