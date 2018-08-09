aba30001070b0f2d829228c8dcc919ba68c85613
opliz <32351129+opliz@users.noreply.github.com>
2018-08-09 09:37:55 +0700

    test

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  api : "http://app-opliz.1d35.starter-us-east-1.openshiftapps.com"
+  // api : "http://app-opliz.1d35.starter-us-east-1.openshiftapps.com"
+  api : "https://app-encee.7e14.starter-us-west-2.openshiftapps.com"
 };
